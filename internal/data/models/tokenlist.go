7a0f37571da02888bd23f7658686fa055d75836a
赵清良 <z_qingliang@163.com>
2022-07-29 06:41:44 +0000

    Zql data

--- internal/data/models/tokenlist.go
+++ internal/data/models/tokenlist.go
@@ -1,19 +1,16 @@
 package models
 
-import "gorm.io/gorm"
-
 type TokenList struct {
-	gorm.Model
-	Id          int
+	BaseModel
 	CgId        string
 	CmcId       int
 	Symbol      string
 	Name        string
-	Chain       string `gorm:"primaryKey;index;index:idx_chain_address"`
-	Address     string `gorm:"primaryKey;index:idx_chain_address"`
+	Chain       string `gorm:"index:,unique,composite:unique_chain_address"`
+	Address     string `gorm:"index:,unique,composite:unique_chain_address"`
 	Description string
 	Logo        string
-	LogoURI     string
 	WebSite     string
 	Decimals    int
+	LogoURI     string
 }
