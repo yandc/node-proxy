b4613a42ac4013f74507c0aae86eb8769c104cba
赵清良 <z_qingliang@163.com>
2023-06-01 00:30:03 +0000

    add rpc timeout

--- internal/service/tokenlist.go
+++ internal/service/tokenlist.go
@@ -22,7 +22,6 @@ func (s *TokenlistService) GetPrice(ctx context.Context, req *pb.PriceReq) (*pb.
 	// 设置接口 3s 超时
 	subctx, cancel := context.WithTimeout(ctx, 3*time.Second)
 	defer cancel()
-
 	price, err := s.uc.GetPrice(subctx, req.CoinNames, req.CoinAddresses, req.Currency)
 	return &pb.PriceResp{
 		Data: price,
